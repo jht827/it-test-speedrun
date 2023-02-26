7f7b74695457fadb1e4b4c4526771beb181eb0b7
jht827 <80994273+jht827@users.noreply.github.com>
2023-02-26 18:33:33 +0800

    Add files via upload

---
+++ windows01.bat
@@ -0,0 +1,8 @@
+md C:\EXAM\windows01\练习
+copy C:\EXAM\windows01\音乐\SummerTime.mp3 C:\EXAM\windows01\作品
+copy C:\EXAM\windows01\音乐\一起摇摆.mp3 C:\EXAM\windows01\作品
+copy C:\it-test-speedrun\P1.psd C:\EXAM\PhotoShop01
+copy C:\it-test-speedrun\py01.py C:\EXAM\Python操作01
+copy C:\it-test-speedrun\电脑时代.wps C:\EXAM\wps文字01
+copy C:\it-test-speedrun\气温与环境.et C:\EXAM\WPS表格01
+copy C:\it-test-speedrun\中华五岳.dps C:\EXAM\WPS演示01
